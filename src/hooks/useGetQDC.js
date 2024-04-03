2dabd60ba1002287b1ef30c0aea96729e1dc6126
monteskier <jmontoliu7@gmail.com>
2024-04-03 22:46:05 +0200

    preparat per github pages 2

--- src/hooks/useGetQDC.js
+++ src/hooks/useGetQDC.js
@@ -10,7 +10,7 @@ export const useGetQDC = ()=>{
 
   const getSeccio = async()=>{
     try {
-      const response = await fetch('assets/dades/seccio.csv');
+      const response = await fetch('../assets/dades/seccio.csv');
       const csvData = await response.text();
 
 
@@ -46,7 +46,7 @@ export const useGetQDC = ()=>{
 
   const getSubSeccio = async()=>{
     try {
-      const response = await fetch('assets/dades/subseccio.csv');
+      const response = await fetch('../assets/dades/subseccio.csv');
       const csvData = await response.text();
 
 
@@ -82,7 +82,7 @@ export const useGetQDC = ()=>{
   
   const getSubSerie = async()=>{
     try {
-      const response = await fetch('assets/dades/subserie.csv');
+      const response = await fetch('../assets/dades/subserie.csv');
       const csvData = await response.text();
 
 
@@ -117,7 +117,7 @@ export const useGetQDC = ()=>{
   }
   const getSerie = async()=>{
    try {
-      const response = await fetch('assets/dades/serie.csv');
+      const response = await fetch('../assets/dades/serie.csv');
       const csvData = await response.text();
 
 
